8f25f1fd32aec607b8b7537c01adf4f60771a991
Melissa Mercado <melissa.a.merc@gmail.com>
2020-08-26 21:01:22 -0400

    fixed

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-ReactDOM.render(<App />, document.getElementById("root"));
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-function Search() {}
+function Search() {
+  return (
+    <div>
+      <form className="form-inline">
+        <input
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
 export default Search;
